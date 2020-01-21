0e3bbb08047f85fb8ae84ef307a2f0b78e0c9d06
MinexAutomation <minex.automation@gmail.com>
2020-01-20 23:24:45 -0800

    Added the user profile directory-based implementation of the IDropboxDirectoryPathProvider service.

--- source/R5T.Bulgaria.Visigothia/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Bulgaria.Visigothia
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Bulgaria.Visigothia/Code/Services/Implementations/UserProfileDropboxDirectoryPathProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Lombardy;
+using R5T.Peristeria;
+using R5T.Visigothia;
+
+
+namespace R5T.Bulgaria.Visigothia
+{
+    public class UserProfileDropboxDirectoryPathProvider : IDropboxDirectoryPathProvider
+    {
+        private IUserProfileDirectoryPathProvider UserProfileDirectoryPathProvider { get; }
+        private IDropboxDirectoryNameConvention DropboxDirectoryNameConvention { get; }
+        public IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public UserProfileDropboxDirectoryPathProvider(
+            IUserProfileDirectoryPathProvider userProfileDirectoryPathProvider,
+            IDropboxDirectoryNameConvention dropboxDirectoryNameConvention,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.UserProfileDirectoryPathProvider = userProfileDirectoryPathProvider;
+            this.DropboxDirectoryNameConvention = dropboxDirectoryNameConvention;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetDropboxDirectoryPath()
+        {
+            var userProfileDirectoryPath = this.UserProfileDirectoryPathProvider.GetUserProfileDirectoryPath();
+
+            var dropboxDirectoryName = this.DropboxDirectoryNameConvention.GetDropboxDirectoryName();
+
+            var dropboxDirectoryPath = this.StringlyTypedPathOperator.GetDirectoryPath(userProfileDirectoryPath, dropboxDirectoryName);
+            return dropboxDirectoryPath;
+        }
+    }
+}
